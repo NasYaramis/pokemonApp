36946abf03f154d97050a2b20986c2c1f45a7eac
Naseyb Yaramis <yaramis.naseyb@gmail.com>
2018-08-20 17:03:06 +0200

    Dit is mijn angular script

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -1,110 +1,72 @@
 'use strict'
+var pokemondb = '../../../pokemon/_all_docs?include_docs=true';
+var pokemon2db = '../../_all_docs?include_docs=true';
 
 angular.module('pokemonApp', ['ngRoute'])
 
-.config(function($routeProvider) {
+pokemonApp.config(function($routeProvider) {
     $routeProvider
         .when('/home', {
             templateUrl: 'assets/views/home.html',
-            controller: 'homeCtrl'
+            controller: 'homeController'
         })
         .otherwise({ redirectTo: '/home' });
 })
 
-.controller('homeCtrl', function ($scope, putCouchdbSrv, pokemonSrv, putCouchdbSrv) {
-    $('#searchButton').on('click', function(e){
-        var date1 = $('#date1Text').val();
-        var date2 = $('#date2Text').val();
-        var BY_DATE = '_view/byDate';
-        var db1 = 'http://localhost:5984/pokemon/';
-        var COUCHDB = '../../' + actor;
-        
-        putCouchdbSrv.getCouchdbData(BY_DATE, date1, date2).then(function(data){
+pokemonApp.controller('homeController', function($scope, pokemonSrv){
+    $('#searchPokemon').on('click', function(){
+        var url = '../../_design/by_owned/_view/owned?startkey="'+$('#date1').val()+'"$endkey="' +$('#date2').val()+'"';
+        pokemonSrv.getPokemonsFromDb(url).then(function succes(data){
+            $scope.pokemons = data;
             console.log(data);
-            if(data.data.rows.length == 0){
-                console.log("actor does not yet exist");
-                pokemonSrv.getIMDBData(IMDB, actor).then(function(data){
-                    console.log(data);
-                    
-                    var pokemons = [];
-                    
-                    var htmlString = '<ul>';
-                    for (var i = 0; i < data.filmography.actor.length; i++) {
-                        pokemons.push(data);
-                    }
-                    var doc = {};
-                    
-                    doc.name = data.name;
-                    doc.type = type;
-                    doc.trainer = trainer;
-                    doc.gender = gender;
-                    doc.owned = owned;
-                    
-                    var json = JSON.stringify(doc);
-                    console.log(json);
-                    
-                    $('#pokemons').html(htmlString);
-                    
-                    putCouchdbSrv.putCouchdbData(COUCHDB, json).then(function(data){
-                        console.log(data);
-                    })
-                })
-            }
-            
+        }, function error(msg){
+            alert('Er is een fout!')
+            console.log(msg);
         });
-    })
-})
-.filter('dateRange', function() {
-    return function( items, fromDate, toDate ) {
-        var filtered = [];
-        //here you will have your desired input
-        console.log(fromDate, toDate);
-        var from_date = Date.parse(fromDate);
-        var to_date = Date.parse(toDate);
-        angular.forEach(items, function(item) {
-            if(item.completed_date > from_date && item.completed_date < to_date) {
-                filtered.push(item);
-            }
+    });
+
+    pokemonSrv.getPokemonsFromDb(pokemondb).then(function success(data){
+        for (var i = 0; i < data.length-1; i++){
+            pokemonSrv.addPokemon(data[i].name, data[i]);
+        }
+    }, function error(msg){
+            alert('Er is een fout bij db!')
+            console.log(msg);
         });
-        return filtered;
-    };
-})
+    });
 
-.service('pokemonSrv', function($http, $q) {
-    this.getPokemons = function(pokemon) {
-        var q = $q.defer();
-        var url = 'http://admin:admin@localhost:5984/pokemon2/';
-
-        $http.get(url)
-            .then(function(data) {
-                var pokemonObj = data.data.results[0].known_for;
-                var movies = [];
-                for (var i = 0; i < pokemonObj.length; i++) {
-                    pokemons.push(pokemonObj[i].id);
-                }
-                q.resolve(pokemons);
-            }, function(err) {
-                q.reject(err);
-            });
-
-        return q.promise;
-    };
-})
-
-.service('putCouchdbSrv', function($http, $q){
-    	
-    var q = $q.defer();
-
-    this.putCouchdbData = function(url, json){
-
-        console.log(url);
-        $http.put(url, json)
+    pokemonApp.service('pokemonSrv', fucntion($http, $q){
+        this.getPokemonsFromDb = function(url){
+            var deferred = $q.defer();
+            var url2 = url;
+            $http.get(url2)
             .then(function(data){
-                q.resolve(data);
-            }, function error(err){
-                q.reject(err);
-                console.log(err);
+                var pokemonDoc = data.data.rows;
+                deferred.resolve(pokemonDoc);
+            }, function(err){
+                deferrred.reject(err);
             });
-        return q.promise;
+            return defered.promise;
+        };
+
+        this.getPokemonsFromDb = function(databaseURL){
+            var deferred = $q.defer();
+            var url = databaseURL;
+            $http.get(url2)
+            .then(function(data){
+                var pokemonDoc = data.data.rows[0].doc;
+                var pokemonArr = [];
+                for (var i = 0; i < pokemonDoc.docs.length; i++){
+                    pokemonArr.push(pokemonDoc.docs[i]);
+                }
+                deferred.resolve(pokemonArr);
+            }, function(err){
+                deferred.reject(err);
+            });
+            return deferred.promise;
+        };
+
+    this.addPokemon = function(key, value){
+        $http.put('../../' + key, value);
     };
-})
+});
